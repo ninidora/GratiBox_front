edb900419ec1b22a40de965f1155d944ed0ff41d
thipereira02 <thiago.spereira02@gmail.com>
2021-11-17 15:07:59 -0300

    Creating test function

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+
+export default function App(){
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact>
+                    <HomePage />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
+}
